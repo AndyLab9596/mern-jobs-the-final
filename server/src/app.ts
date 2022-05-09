2dd216db3fab07cfdccfd99dc0f5825801aca571
andyvy <thienvy95@gmail.com>
2022-05-09 15:38:43 +0700

    in progress fix bug

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import "express-async-errors";
 import dotenv from 'dotenv';
 dotenv.config()
-import connectDb from './db/connectDb';
 import notFoundMiddleware from './middleware/not-found';
 import errorHandlerMiddleware from './middleware/error-handler';
 import authenticateUser from './middleware/authenticateUser';
@@ -10,6 +9,11 @@ import morgan from 'morgan';
 import cors from "cors";
 import authRouter from './routes/authRoutes';
 import jobRouter from './routes/jobRoutes';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import connectDb from './db/connectDb';
+
 
 const app: Application = express();
 
@@ -18,11 +22,16 @@ const app: Application = express();
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
 }
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(cors());
 app.use(express.json());
-
+// app.use(express.static(path.resolve(__dirname, '.././client/build')));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, '.././client/build', 'index.html'))
+})
 
 // Custom Middleware
 app.use(notFoundMiddleware);
