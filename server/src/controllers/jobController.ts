1a5341af7d67d32fd4b312b3852c8a59cbaff20c
andyvy <thienvy95@gmail.com>
2022-05-09 14:27:43 +0700

    pagination

--- server/src/controllers/jobController.ts
+++ server/src/controllers/jobController.ts
@@ -84,10 +84,18 @@ const getAllJobs = async (req: Request, res: Response) => {
         result = result.sort('-position')
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * 10;
+
+    result = result.skip(skip).limit(limit);
 
     const jobs = await result;
+    
+    const totalJobs = await Job.countDocuments(queryObject);
+    const numOfPages = Math.ceil(totalJobs / limit);
 
-    res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+    res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 
 const showStats = async (req: Request, res: Response) => {
